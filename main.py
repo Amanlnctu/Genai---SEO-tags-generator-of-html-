915768fe9b5e1cbb19c390be404ace33d63d4340
Amanlnctu <160702658+Amanlnctu@users.noreply.github.com>
2025-02-23 02:34:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import streamlit as st
 from PIL import Image
 from google import genai
+from dotenv import load_dotenv
+import os
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+# Load environment variables
+load_dotenv()
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=API_KEY)
 st.title("GenAI - Image Tags , Title , Alt generator")
 st.markdown("---")
 st.subheader("Upload your image")
 system_prompts = '''generate  five to six TAGs WHICH SUITS THE IMAGE and a title for the image and one alternative text only and only in this particluar format "ALT : -   , TITLE : -   , TAGS :- " remeber that both the information is for the html alt and title tags for seo ranking it as to be short and precise and not too long just short just the out put of the tags and the title and the alt text  and no other information and no unwanted text   '''
 file = st.file_uploader("Upload an image", type=["jpg", "png"])
 if st.button("Generate"):
-  image = Image.open(file)
-  response = client.models.generate_content(
-      model="gemini-2.0-flash",
-      contents=[image, system_prompts])
-  st.subheader("response : ")
-  print(response.text)
-  st.write(response.text)
+    image = Image.open(file)
+    response = client.models.generate_content(model="gemini-2.0-flash",
+                                              contents=[image, system_prompts])
+    st.subheader("response : ")
+    print(response.text)
+    st.write(response.text)
